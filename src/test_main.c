5b7b742419c5904c8842e898e7c6e5726f77ebd5
Chang <xiangxiang3451@gmail.com>
2025-12-26 21:33:51 +0300

    Add fractal test

--- src/test_main.c
+++ src/test_main.c
@@ -1,6 +1,10 @@
 #include "../YY.utest/utest.h"
 #include "image.h"
+#include "fractal.h"
 
+/*
+ * Test basic image operations
+ */
 UTEST(Image, SetGet)
 {
 	image_p img = create_image(10, 10);
@@ -19,6 +23,102 @@ UTEST(Image, Clear)
 	clear_image(img);
 
 	ASSERT_EQ(0, get_pixel(img, 2, 2));
+
+	free_image(img);
+}
+
+/*
+ * Test image dimensions
+ */
+UTEST(Image, Dimensions)
+{
+	image_p img = create_image(640, 480);
+
+	ASSERT_EQ(640, image_width(img));
+	ASSERT_EQ(480, image_height(img));
+
+	free_image(img);
+}
+
+/*
+ * Test boundary pixel access
+ */
+UTEST(Image, BoundaryPixels)
+{
+	image_p img = create_image(100, 100);
+
+	/* Set the four corner pixels */
+	set_pixel(img, 0, 0, 10);
+	set_pixel(img, 99, 0, 20);
+	set_pixel(img, 0, 99, 30);
+	set_pixel(img, 99, 99, 40);
+
+	ASSERT_EQ(10, get_pixel(img, 0, 0));
+	ASSERT_EQ(20, get_pixel(img, 99, 0));
+	ASSERT_EQ(30, get_pixel(img, 0, 99));
+	ASSERT_EQ(40, get_pixel(img, 99, 99));
+
+	free_image(img);
+}
+
+/*
+ * Test basic Mandelbrot set functionality
+ */
+UTEST(Fractal, MandelbrotBasic)
+{
+	image_p img = create_image(100, 100);
+
+	mandelbrot_fractal(img);
+
+	/* Check whether valid pixel values were generated (0–255) */
+	pixel_data center = get_pixel(img, 50, 50);
+	ASSERT_LE(center, 255);
+	ASSERT_GE(center, 0);
+
+	/* Check corner pixel values */
+	pixel_data corner = get_pixel(img, 0, 0);
+	ASSERT_LE(corner, 255);
+	ASSERT_GE(corner, 0);
+
+	free_image(img);
+}
+
+/*
+ * Test basic Sierpinski carpet functionality
+ */
+UTEST(Fractal, SierpinskiBasic)
+{
+	image_p img = create_image(81, 81);
+
+	sierpinski_fractal(img);
+
+	/*
+	 * The Sierpinski carpet should be a black-and-white image
+	 */
+	pixel_data center = get_pixel(img, 40, 40);
+	ASSERT_TRUE(center == 0 || center == 255);
+
+	/* Check corner area */
+	pixel_data corner = get_pixel(img, 0, 0);
+	ASSERT_TRUE(corner == 0 || corner == 255);
+
+	free_image(img);
+}
+
+/*
+ * Test empty fractal function
+ */
+UTEST(Fractal, EmptyFractal)
+{
+	image_p img = create_image(50, 50);
+
+	set_pixel(img, 25, 25, 100);
+	set_pixel(img, 10, 10, 200);
+
+	empty_fractal(img);
+
+	ASSERT_EQ(0, get_pixel(img, 25, 25));
+	ASSERT_EQ(0, get_pixel(img, 10, 10));
 
 	free_image(img);
 }
